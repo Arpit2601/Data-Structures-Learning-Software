cf35e0fc16b7467c5a8fcf8a0d7c6cd64008e046
LavishGulati <lavish00gulati@gmail.com>
2019-03-16 10:11:23 +0530

    Initialized project

---
+++ DataStructuresLearningSoftware/DataStructuresLearningSoftware/DataStructuresLearningSoftware.cpp
@@ -0,0 +1,18 @@
+// DataStructuresLearningSoftware.cpp : main project file.
+
+#include "stdafx.h"
+#include "MainPage.h"
+
+using namespace DataStructuresLearningSoftware;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew MainPage());
+	return 0;
+}
