33dc8665998ecacea1a0a3f10f3632a0c2999438
LavishGulati <lavish00gulati@gmail.com>
2019-03-27 14:00:31 +0530

    Added Modules table in database

--- DataStructuresLearningSoftware/DataStructuresLearningSoftware/DataStructuresLearningSoftware.cpp
+++ DataStructuresLearningSoftware/DataStructuresLearningSoftware/DataStructuresLearningSoftware.cpp
@@ -22,6 +22,6 @@ int main(array<System::String ^> ^args)
 	Application::SetCompatibleTextRenderingDefault(false); 
 
 	// Create the main window and run it
-	Application::Run(gcnew Searching());
+	Application::Run(gcnew Form1());
 	return 0;
 }
